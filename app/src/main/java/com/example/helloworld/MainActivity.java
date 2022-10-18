3947fb6f780ce47f9f90b3b6d205765456888c42
Nzm <nizam.aboudou@edu.esiee.fr>
2022-10-18 15:35:34 +0200

    Version 2

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+//fjfhfhfj
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.helloworld.MESSAGE";
 
